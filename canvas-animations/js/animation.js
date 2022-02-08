0d2a8c36c5de473bb84437f2a534a765f3d75fee
kchkhikvadze13 <kchkhikvadze@eastsideprep.org>
2022-02-08 10:54:45 -0800

    portfolio website  started

--- canvas-animations/js/animation.js
+++ canvas-animations/js/animation.js
@@ -1,5 +1,72 @@
 let myCamvas = document.getElementById("my-canvas");
 let ctx = myCamvas.getContext("2d");
+//get paragraph items
+let keydownOutput = document.getElementById("keydown-output");
+let keyupOutput = document.getElementById("keyup-output");
+
+
+//when key is pressed 
+function keyPressed(event) {
+    //get the actuall key pressed
+    let key = event.keyCode;
+    keydownOutput.innerHTML = "key down code: " + key;
+
+    //make player stop
+    if (key === 37) {
+        playerXDir = -1;
+    } else if (key === 39) {
+        playerXDir = 1;
+    } else if (key === 38) {
+        playerYDir = -1;
+    } else if (key === 40) {
+        playerYDir = 1;
+    }
+}
+
+function keyReleased(event) {
+    //get the actuall key pressed
+    let key = event.keyCode;
+    keyupOutput.innerHTML = "key up code: " + key;
+
+    //move player
+    if (key === 37) {
+        playerXDir = 0;
+    } else if (key === 39) {
+        playerXDir = 0;
+    } else if (key === 38) {
+        playerYDir = 0;
+    } else if (key === 40) {
+        playerYDir === 0;
+    }
+}
+
+
+
+//player position and movmeent
+let playerX = 250;
+let playerY = 250;
+let playerXDir = 0;
+let playerYDir = 0;
+let playerSpeed = 2;
+
+function drawPlayer() {
+    ctx.fillRect(playerX, playerY, 100, 20);
+}
+
+function movePlayer() {
+    playerX += (playerSpeed * playerXDir);
+    playerY += (playerSpeed * playerYDir);
+}
+
+function refreshPlayer() {
+    ctx.clearRect(0, 0, 500, 500);
+    movePlayer();
+    drawPlayer();
+}
+
+
+
+
 
 //object position
 //let xPosition = 0;
@@ -55,4 +122,5 @@ function moveVertical() {
 // #3 ball bouncing horizontally
 
 
-setInterval(moveVertical, 5)
+//setInterval(moveVertical, 5);
+setInterval(refreshPlayer, 30);
